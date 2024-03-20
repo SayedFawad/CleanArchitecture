06df236335ce066bd0863f6f40a3b5b87538297d
Sayed Fawad Hakimi <favad.hakimi@gmail.com>
2024-03-20 11:43:54 +0430

    Add project files.

---
+++ CleanMovie.API/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanMovie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _service;
+
+        public MoviesController(IMovieRepository service)
+        {
+            _service = service;
+        }
+
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            var moviesFromService = _service.GetAllMovies();
+            return Ok(moviesFromService);
+        }
+
+    
+    }
+}
---
+++ CleanMovie.API/Program.cs
@@ -0,0 +1,30 @@
+using CleanMovie.Application;
+using CleanMovie.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+//builder.Services.AddScoped<IMovieService, MovieService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
